59145a6b02b5c7ed290b88b11239996b728ab27c
simiaosimis <simiaosimis@hotmail.com>
2015-04-07 19:55:58 -0300

    Starting Level 2

--- game/js/JammedScript/jammedscript.js
+++ game/js/JammedScript/jammedscript.js
@@ -14,6 +14,7 @@ var i = 0;
 var pressedKeys = [];
 
 var state = 0;
+var level = 1;
 
 var ENDGAME_VICTORY = false;
 var ENDGAME_GAMEOVER = false;
@@ -49,7 +50,7 @@ var JUMPSPEED = (-830);
 var player_direction = '';
 
 var LEFT_CAMERA_PIN = 200;
-var RIGHT_CAMERA_PIN = 600;
+var RIGHT_CAMERA_PIN = 400;
 
 var BACKGROUND_WIDTH = 2000;
 
@@ -465,7 +466,7 @@ function renderHP(){
 	var gearhp = new Image();
 	gearhp.src = "res/gear_HP.png";
 	
-	d.drawImage(gearhp, 30, 30, 80, 80);
+	//d.drawImage(gearhp, 30, 30, 80, 80);
 	
 	var gearhit = new Image();
 	gearhit.src = "res/gear_HIT.png";
@@ -919,8 +920,8 @@ function EnemyCopter(x, y, range){
 function createEnemy(xpos,ypos,range){
 	var x = xpos*SPRITE_SIZE;
 	var y = (ypos*SPRITE_SIZE) + 64;
-	var position = xpos*7 + ypos;
-	entities[position] = new EnemyCopter(x,y,range);
+	//var position = xpos*7 + ypos;
+	entities[entities.length] = new EnemyCopter(x,y,range);
 }
 // Jamming from file: 4.2.0_Box.js
 /* *************************
@@ -966,8 +967,8 @@ function Box(x, y, mutant,sprite){
 function createBox(xpos,ypos,mutant,sprite){
 	var x = xpos*SPRITE_SIZE;
 	var y = (ypos*SPRITE_SIZE) + 64;
-	var position = xpos*7 + ypos;
-	entities[position] = new Box(x,y,mutant,sprite);
+	//var position = xpos*7 + ypos;
+	entities[entities.length] = new Box(x,y,mutant,sprite);
 }
 // Jamming from file: 4.2.1_Spike.js
 /* *************************
@@ -1043,8 +1044,8 @@ function Spike(x,y,mutant,sprite,type){
 function createSpike(xpos,ypos,mutant,sprite,type){
 	var x = xpos*SPRITE_SIZE;
 	var y = (ypos*SPRITE_SIZE) + 64;
-	var position = xpos*7 + ypos;
-	entities[position] = new Spike(x,y,mutant,sprite,type);
+	//var position = xpos*7 + ypos;
+	entities[entities.length] = new Spike(x,y,mutant,sprite,type);
 }
 // Jamming from file: 4.2.2_Gear.js
 /* *************************
@@ -1074,8 +1075,8 @@ function Gear(x,y){
 function createGear(xpos,ypos){
 	var x = xpos*SPRITE_SIZE;
 	var y = (ypos*SPRITE_SIZE) + 64;
-	var position = xpos*7 + ypos;
-	entities[position] = new Gear(x,y);
+	//var position = xpos*7 + ypos;
+	entities[entities.length] = new Gear(x,y);
 }
 // Jamming from file: 4.2.3_Portal.js
 /* *************************
@@ -1111,8 +1112,8 @@ function Portal(x,y){
 function createPortal(xpos,ypos){
 	var x = xpos*SPRITE_SIZE;
 	var y = (ypos*SPRITE_SIZE) + 64;
-	var position = xpos*7 + ypos;
-	entities[position] = new Portal(x,y);
+	//var position = xpos*7 + ypos;
+	entities[entities.length] = new Portal(x,y);
 }
 // Jamming from file: 4.3_Scenary.js
 /* *************************
@@ -1506,7 +1507,7 @@ function initialize(){
 	createBox(8, 7, false, metal_box);
 	
 	//puzzle 2
-	createGear(17,4);
+	createGear(11,6);
 	createBox(9, 7, true, boxgear_circle_sprite);
 	createBox(15, 6, false, box_circle_sprite);
 	createBox(15, 7, false, metal_box);
@@ -1563,7 +1564,7 @@ function initialize(){
 	createSpike(28,0,false,downspike_middle, MIDDLE_SPIKE);
 	createSpike(29,0,false,downspike_end, END_SPIKE);
 	
-	createEnemy(28,1,160);
+	createEnemy(28,1,260);
 	createGear(29,2);
 	
 	//right wall
@@ -1578,9 +1579,71 @@ function initialize(){
 	
 	menu = d.createPattern(resources.get('res/menu.png'), 'repeat');
 	
-	
+
 	lastTime = window.performance.now();
     main();
+}
+
+function initializeLevel2(quant){
+	music.play();
+
+	entities.splice(0,entities.length);
+	
+	//left wall
+	createBox(0, 0, false, box_triangle_sprite);
+	createBox(0, 1, false, box_triangle_sprite);
+	createBox(0, 2, false, box_triangle_sprite);
+	createBox(0, 3, false, box_triangle_sprite);
+	createBox(0, 4, false, box_triangle_sprite);
+	createBox(0, 5, false, box_triangle_sprite);
+	createBox(0, 6, false, box_triangle_sprite);
+	createBox(0, 7, false, box_triangle_sprite);
+	createBox(1, 7, false, box_triangle_sprite);
+	createBox(2, 7, false, box_triangle_sprite);
+	createBox(3, 7, false, box_triangle_sprite);
+	createBox(4, 7, false, box_circle_sprite);
+	createBox(5, 0, false, box_triangle_sprite);
+	createBox(5, 1, false, box_triangle_sprite);
+	createBox(5, 2, false, box_triangle_sprite);
+	createBox(5, 3, false, box_triangle_sprite);
+	
+	//puzzle 1
+	createSpike(5,7,true,spikegear_triangle_start,START_SPIKE);
+	createSpike(6,7,true,spikegear_triangle,MIDDLE_SPIKE);
+	createSpike(7,7,true,spikegear_triangle,MIDDLE_SPIKE);
+	createSpike(8,7,true,spikegear_triangle,MIDDLE_SPIKE);
+	createSpike(9,7,true,spikegear_triangle,MIDDLE_SPIKE);
+	createSpike(10,7,true,spikegear_triangle_end,END_SPIKE);
+	createBox(5, 4, true, spike_circle_start, START_SPIKE);
+	createBox(6, 4, true, spike_circle, MIDDLE_SPIKE);
+	createBox(7, 4, true, spike_circle, MIDDLE_SPIKE); 
+	createBox(8, 4, true, spike_circle, MIDDLE_SPIKE);
+	createBox(9, 4, true, spike_circle, MIDDLE_SPIKE);
+	createBox(10, 4, true, spike_circle_end, END_SPIKE);
+	//puzzle 2
+	
+	//puzzle 3
+	
+	//puzzle 4
+
+	//puzzle 5
+	
+	createEnemy(28,1,160);
+	createGear(29,2);
+	
+	//right wall
+	createBox(30, 0, true, metal_box);
+	createBox(30, 1, true, metal_box);
+	createBox(30, 2, true, metal_box);
+	createBox(30, 3, true, metal_box);
+	createBox(30, 4, true, metal_box);
+	createBox(30, 5, true, metal_box);
+	createBox(30, 6, true, metal_box);
+	createBox(30, 7, true, metal_box);
+
+	player.gearsCollected = 0;
+	scenary = new Scenary(0, 0);
+	paused = false;
 }
 
 // The main game loop
@@ -1589,16 +1652,15 @@ function main() {
     var now = window.performance.now();
     var dt = (now - lastTime) / 1000.0;
 	
-	update(dt);
-	render();
-	
 	if(!paused){
+		update(dt);
+		render();
 		gameTime += dt;
 	}
 	
 	lastTime = now;
 	requestAnimFrame(main);
-	
+
 	//drawing menu
 	if(state == STATE_MENU){
 		paused = true;
@@ -1621,7 +1683,18 @@ function main() {
 		d.clearRect(0, 0, canvas.width, canvas.height);
 		d.drawImage(helpScreen,0,0,canvas.width, canvas.height);
 	}
-	else if(ENDGAME_VICTORY && state == STATE_GAME ){
+	else if(ENDGAME_VICTORY && state == STATE_GAME && level == 1){
+		paused = true;
+		daux.clearRect(0, 0, canvas.width, canvas.height);
+		d.clearRect(0, 0, canvas.width, canvas.height);
+		initializeLevel2(entities.length);
+		level = 2;
+		state = STATE_GAME;
+		player.x = 50;
+		player.y = 100;
+		ENDGAME_VICTORY = false;
+	}
+	else if(ENDGAME_VICTORY && state == STATE_GAME && level == 2){
 		paused = true;
 		daux.clearRect(0, 0, canvas.width, canvas.height);
 		d.clearRect(0, 0, canvas.width, canvas.height);
