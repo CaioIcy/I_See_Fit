b9d79a67492b6e62d6c4621f0d6740206fc96915
CaioIcy <caio.nardelli@hotmail.com>
2015-04-11 18:21:19 -0300

    changing spikes to metal boxes in level 2

--- game/js/8_2_Level2.js
+++ game/js/8_2_Level2.js
@@ -13,14 +13,14 @@ function initializeLevel2(){
 	createBox(0, 5, false, metal_box);
 	createBox(0, 6, false, metal_box);
 	createBox(0, 7, false, metal_box);
-	createBox(1, 7, false, box_triangle_sprite);
-	createBox(2, 7, false, box_triangle_sprite);
-	createBox(3, 7, false, box_triangle_sprite);
+	createBox(1, 7, false, metal_box);
+	createBox(2, 7, false, metal_box);
+	createBox(3, 7, false, metal_box);
 	createBox(4, 7, false, box_circle_sprite);
-	createBox(5, 0, false, box_triangle_sprite);
-	createBox(5, 1, false, box_triangle_sprite);
-	createBox(5, 2, false, box_triangle_sprite);
-	createBox(5, 3, false, box_triangle_sprite);
+	createBox(5, 0, false, metal_box);
+	createBox(5, 1, false, metal_box);
+	createBox(5, 2, false, metal_box);
+	createBox(5, 3, false, metal_box);
 	
 	//puzzle 1
 	createSpike(5,7,true,spikegear_triangle_start,START_SPIKE);
